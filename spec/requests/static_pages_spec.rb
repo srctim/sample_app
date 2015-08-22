e0fd77819539b750f9991fbf399ae20c788ccc32
srctim <zaz2004@ua.fm>
2015-08-22 21:27:22 +0300

    add red tests for title (home, help, about)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+
   end
 
   describe "Help page" do
@@ -13,6 +19,12 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help Page')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+
   end
 
   describe "About page" do
@@ -20,6 +32,13 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+    end
+
   end
+
 
 end
