5f862dd78ab14b2dc5a72848940e99e61177589d
srctim <zaz2004@ua.fm>
2015-08-22 22:15:56 +0300

    add page about using BDD (TDD)

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   #add route [GET] "/static_pages/about"
   get '/static_pages/about'
   get 'static_pages/help'
-
+  #/static_pages/contact
+  get '/static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
