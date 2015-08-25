1f490dc4044747d0fbca73ec2113b6db0e0d9e2b
srctim <zaz2004@ua.fm>
2015-08-25 16:07:51 +0300

    add routes for pages: about, help, /, contact + tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  #add route [GET] "/static_pages/about"
-  get '/static_pages/about'
-  get 'static_pages/help'
-  #/static_pages/contact
-  get '/static_pages/contact'
+  #get 'static_pages/home'
+  #get '/static_pages/about'
+  #get 'static_pages/help'
+  #get '/static_pages/contact'
+
+  # about_path -> '/about'
+  # about_url  -> 'http://localhost:3000/about'
+
+  root 'static_pages#home'
+  #match 'root',to: 'static_pages#home', via: 'get'
+  match 'about',to: 'static_pages#about', via: 'get'
+  match 'help',to: 'static_pages#help', via: 'get'
+  match 'contact',to: 'static_pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
