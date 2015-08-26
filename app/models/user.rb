5a94d42741192a67cae1c1d9541f5e2da00c8228
srctim <zaz2004@ua.fm>
2015-08-26 14:48:59 +0300

    add validation for unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   #validates(:name, presence: true)
   validates :name, length: {maximum: 50}, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
