23e84ff8c6659018109d9af31d03933612c19169
srctim <zaz2004@ua.fm>
2015-08-26 14:21:15 +0300

    add test for validation length name and implemented it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   #validates(:name, presence: true)
-  validates :name, presence: true
+  validates :name, length: {maximum: 50}, presence: true
   validates :email, presence: true
 end
