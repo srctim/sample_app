b091f200c0cf97074ccf383d17eff39b24d8c94d
srctim <zaz2004@ua.fm>
2015-08-27 11:48:11 +0300

    add regexp for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ActiveRecord::Base
   before_save { email.downcase! }
   validates :name, length: {maximum: 50}, presence: true
   validates :email, presence: true, uniqueness: {case_sensitive: false}
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
   has_secure_password
