70f479b1632b3657922a8f66b77d71bacc6cfb7d
srctim <zaz2004@ua.fm>
2015-08-27 09:59:47 +0300

    add tests for password and password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+                            password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -58,4 +62,13 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 end
