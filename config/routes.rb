953e9b0afb53f2069569320fe78a6b19de21b46d
srctim <zaz2004@ua.fm>
2015-08-31 15:09:58 +0300

    add matches for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   match 'help'   , to: 'static_pages#help'   , via: 'get'
   match 'contact', to: 'static_pages#contact', via: 'get'
 
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
