286627b9837cbfb80c445623729e60e0e3667272
srctim <zaz2004@ua.fm>
2015-08-31 12:38:23 +0300

    add test for signup page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,12 +11,12 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
-  # describe "signup page" do
-  #   before { visit signup_path }
-  #
-  #   it { should have_content('Sign up') }
-  #   it { should have_title(full_title('Sign up')) }
-  # end
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 
   describe "signup page" do
 
