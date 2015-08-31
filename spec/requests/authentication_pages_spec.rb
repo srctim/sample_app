e5414d89e7909bc912b57b2ea1d7d865eb06e9e4
srctim <zaz2004@ua.fm>
2015-08-31 15:29:10 +0300

    add test for fail sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,15 @@ describe "Authentication" do
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
   end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+    end
+  end
 end
