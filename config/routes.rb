db9cba3c8b544457513da1c16996c8ac95c1ebb3
srctim <zaz2004@ua.fm>
2015-08-31 15:13:11 +0300

    add actions: new, create, destroy in session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+
   match 'signup' , to: 'users#new'           , via: 'get'
 
   match '/signin',  to: 'sessions#new',         via: 'get'
